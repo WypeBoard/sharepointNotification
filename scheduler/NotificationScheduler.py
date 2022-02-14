a6abf75c9840ec55acd52cf0a73dc60d96163ba9
Nikolaj Bové Højholt <nbh@netcompany.com>
2022-02-14 10:43:13 +0100

    Making small changes to the program, added the readme and better config description

--- scheduler/NotificationScheduler.py
+++ scheduler/NotificationScheduler.py
@@ -20,15 +20,18 @@ def main():
 
     logger.debug(f'fetching sharepoint view data')
     sharepoint_notifications = SharepointService.get_cases_from_notification_view()
+    if len(sharepoint_notifications) == 0:
+        logger.info(f'The view is currently empty\n')
+        return
+
     logger.info(f'There is currently {len(sharepoint_notifications)} cases in the view')
     logger.info(f'{get_sharepoint_notification_description(sharepoint_notifications)}')
 
     logger.debug(f'fetching database entries')
     db_notifications = NotificationService.get_all_entities()
     grouping = NotificationGrouping(sharepoint_notifications, db_notifications)
-    logger.info(f'{grouping.get_notification_number()} out of {len(sharepoint_notifications)} is being reported')
+    logger.info(f'{grouping.get_notification_number()} out of {len(sharepoint_notifications)} is being reported\n')
     if grouping.get_notification_number() != 0:
         toast = ToastService.create_toast(grouping)
-        logger.debug('Update database with new values')
         NotificationService.update_and_persist(db_notifications, grouping)
         ToastService.wait_for_thread_to_close(toast)
